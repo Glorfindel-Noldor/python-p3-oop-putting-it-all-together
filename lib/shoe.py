25fbb997950250c708a00e72c7c5f3e08d87c269
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-25 09:19:29 -0500

    decorators and encapsulations

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, arg_brand, arg_size):
+        self.brand = arg_brand
+        self._size = arg_size
+
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else: print('size must be an integer')
+    def cobble(self):
+        """says that the shoe has ben repaired"""
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
